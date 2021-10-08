5eb15fd3c5419f2bd8e296835609c53d1a8e4cbf
Beata <beata.koziel@skycash.com>
2021-10-08 08:58:54 +0200

    initial

---
+++ src/main/java/com/engine/realestatesearchapp/RealEstateSearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.engine.realestatesearchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateSearchAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateSearchAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/engine/realestatesearchapp/RealEstateSearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.realestatesearchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateSearchAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
